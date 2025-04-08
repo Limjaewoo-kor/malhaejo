2683111259bae0ee9d7527e6f931b75d22d708ad
Ljw <ljw3944@naver.com>
2025-04-08 15:40:51 +0900

    Update cors update

--- main.py
+++ main.py
@@ -19,7 +19,8 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                   "http://localhost:3000"],  # Vite일 경우
+                   "http://localhost:3000",
+                   "https://malhaejo.vercel.app"],  # Vite일 경우
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
